0fd5077aa52abc6ad90171473011c70f56f62677
charankamal20 <charanksingh20@gmail.com>
2024-10-03 14:17:53 +0530

    stuff

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -9,12 +9,12 @@ export const youtube_api = axios.create({
 });
 
 export const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1`,
+  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api`,
   withCredentials: true,
 });
 
 export const auth_api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1`,
+  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api`,
   withCredentials: true,
 });
 
